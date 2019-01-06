9d288edd91d26fd7d3e69dd87c86f8c30ce24a06
rubenavone <navone_ruben@msn.com>
2019-01-06 17:39:23 +0100

    Modif sur le programme HelloWorld

--- learning-java/learn-java-01HelloWorld/src/Hello.java
+++ learning-java/learn-java-01HelloWorld/src/Hello.java
@@ -7,8 +7,9 @@ public class Hello {
 	public static void main (String[] args){
 	
 		JFrame frame = new JFrame( "Hello, Java!");
-		JLabel label = new JLabel("Hello, Java!", JLabel.CENTER);
-		frame.add(label);
+		//JLabel label = new JLabel("Hello, Java!", JLabel.CENTER);
+		//frame.add(label);
+		frame.add(new HelloComponent());
 		frame.setSize( 300, 300);
 		frame.setVisible( true );
 
---
+++ learning-java/learn-java-01HelloWorld/src/HelloComponent.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+class HelloComponent extends JComponent {
+
+	public void paintComponent(Graphics g) {
+		
+		g.drawString("Hello, Java!", 125, 95);
+	
+	}
+}

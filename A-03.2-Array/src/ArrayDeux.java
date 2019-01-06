71163c5ba67338eeff632b6b8a731d93377dc812
rubenavone <navone_ruben@msn.com>
2019-01-06 22:34:10 +0100

    Exo 03.2 finis

--- A-03.2-Array/src/ArrayDeux.java
+++ A-03.2-Array/src/ArrayDeux.java
@@ -1,23 +1,33 @@
-  public class Array {
-
-                public static void {
-                        /*
-                        1.Déclarer les deux tableaux ci-dessus:
-                        2.Créer une fonction permettant d’afficher les éléments de chaque tableau ligne par ligne.
-                        2.Créer un programme de Test (main)*/
 
 
-                        for(int i = 0; i < tableau1.length; i++){
+class Array {
 
-                          for(int j = 0; j < tableau1[i].length; j++){
+    public static void main(String[] args){
+         /*
+	     1.Déclarer les deux tableaux ci-dessus:
+	     2.Créer une fonction permettant d’afficher les éléments de chaque tableau ligne par ligne.
+	     3.Créer un programme de Test (main)*/
 
-                                System.out.print(tableau1[i][j] + " "); 
 
-                          }
-                                System.out.println("");
-                        }
+        
+	System.out.println("Affichage des Tableaux via une classe tierse");
 
-                }
+        int tableau1[][] = {{2 , 4 , 5}, {3 , 33, 32}};
+	
+        Affiche.afficheArray(tableau1);
 
-        }
-~         
+        int tableau2[][] = {{3}, {3 , 4 , 5}, {3 , 5}, {3 , 1 , 0}};
+	
+	System.out.println("Affichage du second talbeau");
+
+	Affiche.afficheArray(tableau2);
+
+            
+
+     }
+
+
+}
+
+
+
